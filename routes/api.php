e0a47381545aa300525c131d2f7c22c7d4333f5e
EnzoAmarilla <enzo100amarilla@gmail.com>
2023-12-07 17:24:48 -0300

    test mail EH confirm R

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,11 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ReservationController;
 use App\Http\Controllers\UserController;
+use App\Mail\confirmReservationMailable;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Artisan;
-
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -51,4 +53,33 @@ Route::get('/clear-cache', function() {
     return response()->json([
         "message" => "Cache cleared successfully"
     ]);
+});
+
+Route::get('test_mail',  function(){
+    $data = [
+        'reservation_number' => '123',
+        'name' => 'test',
+        'last_name' => 'test',
+        'check_in' => 'test',
+        'check_out' => 'test',
+        'number_of_passengers' => 4,
+        'email' => 'test',
+    ];
+
+    try {
+        // Mail::to('sl.larramendy@gmail.com')->send(new confirmReservationMailable($data));
+        Mail::to('enzo100amarilla@gmail.com')->send(new confirmReservationMailable($data));
+    } catch (Exception $error) {
+        Log::debug([
+            "message"=> "Proceso correcto. Error en envio de mail",
+            "line" => $error->getLine(),
+            "error" => $error->getMessage(),
+        ]);
+
+        return response([
+            "message" => "Proceso correcto. Error al enviar resumen a su casilla de mail",
+            "status" => 600,
+            "error" => $error->getMessage(),
+        ], 200);
+    }
 });
